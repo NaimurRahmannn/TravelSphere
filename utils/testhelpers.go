29ee58cdb5ed92afff6cb3c1f8f0703eb4420c27
NaimurRahmann <niamurreiman@gmail.com>
2026-06-09 14:12:03 +0600

    feat: add a small exported test helper in the utils package that lets other packages redirect the base URLs.

---
+++ utils/testhelpers.go
@@ -0,0 +1,23 @@
+package utils
+
+import "net/http"
+
+func SetRestCountriesBaseURL(url string) func() {
+	original := restCountriesBase
+	restCountriesBase = url
+	return func() { restCountriesBase = original }
+}
+
+
+func SetOpenTripMapBaseURL(url string) func() {
+	original := openTripMapBase
+	openTripMapBase = url
+	return func() { openTripMapBase = original }
+}
+
+
+func SetHTTPClient(c *http.Client) func() {
+	original := httpClient
+	httpClient = c
+	return func() { httpClient = original }
+}
